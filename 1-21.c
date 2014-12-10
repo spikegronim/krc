c4d70f28c1e2e3f0db9b1d7eb9c6e4e17d6045d7
urthbound <jeffowler@gmail.com>
2014-12-09 22:42:14 -0500

    first tab works ?

--- 1-21.c
+++ 1-21.c
@@ -7,12 +7,15 @@ int main()
 {
     int loc = 0;
     int count = 0;
+    int spot = 0;
     char c;
 
     while ((c = getchar()) != EOF) {
         if (c == '\n') {
             putchar(c);
             loc = 0;
+            spot = 0;
+            count = 0;
         } else if (c == '\t') {
             count +=  (TABWIDTH - (loc % TABWIDTH));
             loc +=  (TABWIDTH - (loc % TABWIDTH));
@@ -21,12 +24,18 @@ int main()
             loc++;
         } else {
             if (count != 0) {
+                if (count > TABWIDTH - (spot % TABWIDTH)) {
+                    putchar('\t');
+                    count -= TABWIDTH - (spot % TABWIDTH);
+                }
+
                 for (int i = 0; i < count; i++)
                     putchar(' ');
                 count = 0;
             }
             putchar(c);
             loc++;
+            spot = loc;
         }
     }
     return 0;
